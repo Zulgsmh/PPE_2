fa05cc7affd67af4e046d6c3122370b3eb70557d
Zulgsmh <lorem1291@gmail.com>
2019-01-29 14:21:17 +0100

    2eme commit modif form/fichier pour doc

--- src/Form/LignefraisforfaitType.php
+++ src/Form/LignefraisforfaitType.php
@@ -3,21 +3,47 @@
 namespace App\Form;
 
 use App\Entity\Lignefraisforfait;
+use App\Entity\FraisForfait;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormView;
+
 
 class LignefraisforfaitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('idvisiteur')
-            ->add('mois')
-            ->add('idfraisforfait')
+		    ->add('idvisiteur', EntityType::class, 
+                 array('class' => 'App\Entity\Visiteur', 
+                        'label' => 'idvisiteur :',
+                         'choice_label' => function($id){
+                            return $id->getId();
+                        }))
+			 ->add('mois')
+			 ->add('idfraisforfait', EntityType::class, 
+            array('class' => 'App\Entity\Fraisforfait', 
+                   'label' => 'idfraisforfait :',
+                    'choice_label' => function($idfraisforfait){
+                       return $idfraisforfait->getId();
+                    
+                   },
+                  'choice_value' => function (FraisForfait $f = null) {
+                    return $f ? $f->getMontant() : '';
+                    },
+                   'placeholder' => 'Choississez ...'))
             ->add('quantite')
             ->add('montant')
+            ->add('save',SubmitType::class)
         ;
+        
+      
     }
 
     public function configureOptions(OptionsResolver $resolver)
